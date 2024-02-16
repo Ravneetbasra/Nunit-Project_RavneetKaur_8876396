2611aafb07414131ee826f89d79d00cea48bd302
Ravneetbasra <ravneetkaurbasra@gmail.com>
2024-02-15 20:20:43 -0500

    Commiting LAB 4

---
+++ TestProject1/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System.Xml.Linq;
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if (((firstAngle + secondAngle + thirdAngle) == 180) && (firstAngle > 0 && secondAngle > 0 && thirdAngle > 0))
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+
+   
+        public static string ValidScaleneTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if((firstAngle != secondAngle && thirdAngle != secondAngle && thirdAngle != firstAngle) && ((firstAngle + secondAngle + thirdAngle) == 180))
+            {
+                result = "Triangle is Scalene";
+            }
+            else
+            {
+                result = "Triangle is not Scalene";
+            }
+            return result;
+        }
+
+        public static string ValidIsocelesTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle == secondAngle || secondAngle == thirdAngle || thirdAngle == firstAngle) && ((firstAngle + secondAngle + thirdAngle) == 180))
+            {
+                result = "Triangle is Isosceles";
+            }
+            else
+            {
+                result = "Triangle is not Isosceles";
+            }
+            return result;
+        }
+
+        public static string ValidEquilateralTriangle(int firstAngle, int secondAngle, int
+       thirdAngle)
+        {
+            string result;
+            if ((firstAngle == secondAngle && secondAngle == thirdAngle && thirdAngle == firstAngle) && (firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "Triangle is Equilateral";
+            }
+
+            else
+            {
+                result = "Triangle is not Equilateral";
+            }
+            return result;
+        }
+
+        public static string ValidRightTriangle(int firstAngle, int secondAngle, int
+      thirdAngle)
+        {
+            string result;
+            if ((firstAngle == 90 || secondAngle == 90 || thirdAngle == 90) && ((firstAngle + secondAngle + thirdAngle) == 180))
+            {
+                result = "Triangle is Right Triangle";
+            }
+
+            else
+            {
+                result = "Triangle is not Right Triangle";
+            }
+            return result;
+        }
+
+    }
+}
+
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,302 @@
+using NUnit.Framework;
+namespace NUnitDemonstration.Tests
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        /* This Test will verify whether it is valid triangle when sum of firstAngle, secondAngle and thirdAngle  have sum of 180.
+            Here the firstAngle == secondAngle == thirdAngle
+        */
+        [Test]
+        public void ValidTriangle_ValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+
+        /* This Test will verify whether it is valid triangle when sum of firstAngle, secondAngle and thirdAngle  have sum of 180.
+            Here the firstAngle != secondAngle != thirdAngle
+        */
+
+        [Test]
+        public void ValidTriangle_ValidInput_ReturnsValidMessage2()
+        {
+            // Arrange
+            int firstAngle = 50;
+            int secondAngle = 70;
+            int thirdAngle = 60;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+
+        /* This Test will verify whether it is valid triangle when sum of firstAngle, secondAngle and thirdAngle  have sum of 180.
+            Here the sum of all angles is not equal to 180. So, it is not a valid Triangle
+        */
+
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage()
+        {
+            // Arrange
+            int firstAngle = 40;
+            int secondAngle = 70;
+            int thirdAngle = 50;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is NOT valid.", result);
+        }
+      
+
+        /* This Test will verify whether it a valid triangle or not.
+         * Valid Triangle must not have any negative angle, so for the below test, it is not a valid Triangle
+        */
+
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage2()
+        {
+            // Arrange
+            int firstAngle = -40;
+            int secondAngle = -70;
+            int thirdAngle = 50;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is NOT valid.", result);
+        }
+
+        /* This Test will verify whether it a valid Scalene triangle or not
+         * Scalene Triangle must have different Angles and sum of the angles still must be 180.
+         * Below Test is a valid Scalene Triangle.
+        */
+        [Test]
+        public void ValidScaleneTriangle_ValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 50;
+            int thirdAngle = 70;
+
+
+            // Act
+            string result = Triangle.ValidScaleneTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is Scalene", result);
+        }
+
+
+        /* This Test will verify whether it a valid Scalene triangle or not
+         * Scalene Triangle must have different angles and sum of the angles still must be 180.
+         * Below Test is a invalid scalene as two angles have same value.
+        */
+
+        [Test]
+        public void InValidScaleneTriangle_InValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 50;
+            int secondAngle = 50;
+            int thirdAngle = 80;
+
+
+            // Act
+            string result = Triangle.ValidScaleneTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Scalene", result);
+        }
+
+        /* This Test will verify whether it a valid Scalene triangle or not
+        * Scalene Triangle must have different angles and sum of the angles still must be 180.
+        * Below Test is a invalid scalene as sum of three angles is greater than 180 which means it is an invalid Triangle
+       */
+
+        [Test]
+        public void ValidScaleneTriangle_InValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 80;
+
+
+            // Act
+            string result = Triangle.ValidScaleneTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Scalene", result);
+        }
+
+        /* This Test will verify whether it a valid Isosceles triangle or not
+        * Isosceles Triangle must have same two angles and sum of the angles still must be 180.
+        * Below Test is a valid Isosceles as it has two angles with the same value
+       */
+
+        [Test]
+        public void ValidIsoscelesTriangle_ValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 70;
+            int secondAngle = 70;
+            int thirdAngle = 40;
+            // Act
+            string result = Triangle.ValidIsocelesTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is Isosceles", result);
+        }
+
+        /* This Test will verify whether it a valid Isosceles triangle or not
+        * Isosceles Triangle must have same two angles and sum of the angles still must be 180.
+        * Below Test is a Invalid Isosceles as it has each different angle
+       */
+
+
+        [Test]
+        public void InvalidIsoscelesTriangle_InvalidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 70;
+            int thirdAngle = 50;
+            // Act
+            string result = Triangle.ValidIsocelesTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Isosceles", result);
+        }
+
+
+        /* This Test will verify whether it a valid Isosceles triangle or not
+        * Isosceles Triangle must have same two angles and sum of the angles still must be 180.
+        * Below Test is a Invalid Isosceles as the sum of all angles is greater than 180.
+       */
+
+        [Test]
+        public void ValidIsoscelesTriangle_InvalidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 80;
+            int secondAngle = 80;
+            int thirdAngle = 50;
+            // Act
+            string result = Triangle.ValidIsocelesTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Isosceles", result);
+        }
+
+        /* This Test will verify whether it a valid Equilateral triangle or not
+        * Equilateral Triangle must have all angles  as same and sum of the angles still must be 180.
+        * Below Test is a valid Equilateral as the three angles have same value
+       */
+
+        [Test]
+        public void ValidEquilateralTriangle_ValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            // Act
+            string result = Triangle.ValidEquilateralTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is Equilateral", result);
+        }
+
+        /* This Test will verify whether it a valid Equilateral triangle or not
+        * Equilateral Triangle must have all angles  as same and sum of the angles still must be 180.
+        * Below Test is a Invalid Equilateral as the three angles have sum greater than 180.
+       */
+
+        [Test]
+        public void ValidEquilateralTriangle_InValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 40;
+            int secondAngle = 40;
+            int thirdAngle = 40;
+            // Act
+            string result = Triangle.ValidEquilateralTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Equilateral", result);
+        }
+
+        /* This Test will verify whether it a valid Equilateral triangle or not
+        * Equilateral Triangle must have all angles  as same and sum of the angles still must be 180.
+        * Below Test is a Invalid Equilateral as the three angles have each different value.
+       */
+
+
+        [Test]
+        public void InValidEquilateralTriangle_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 70;
+            int thirdAngle = 50;
+            // Act
+            string result = Triangle.ValidEquilateralTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Equilateral", result);
+        }
+
+        /* This Test will verify whether it a valid Right triangle or not
+        * Right Triangle must have atleast one angle as 90 and sum of the angles still must be 180.
+        * Below Test is a Right Equilateral as the one of the angle has 90 value
+       */
+
+        [Test]
+        public void ValidRightTriangle_ValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 90;
+            int secondAngle = 45;
+            int thirdAngle = 45;
+            // Act
+            string result = Triangle.ValidRightTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is Right Triangle", result);
+        }
+
+        /* This Test will verify whether it a valid Right triangle or not
+        * Right Triangle must have atleast one angle as 90 and sum of the angles still must be 180.
+        * Below Test is a Invalid Right Equilateral as the no angle has a value of 90.
+       */
+
+        [Test]
+        public void InValidRightTriangle_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 55;
+            int secondAngle = 80;
+            int thirdAngle = 45;
+            // Act
+            string result = Triangle.ValidRightTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Right Triangle", result);
+        }
+
+
+        /* This Test will verify whether it a valid Right triangle or not
+        * Right Triangle must have atleast one angle as 90 and sum of the angles still must be 180.
+        * Below Test is a Invalid Right Equilateral as the sum of the angle is not equal to 180.
+       */
+        [Test]
+        public void ValidRightTriangle_InValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 90;
+            int secondAngle = 80;
+            int thirdAngle = 80;
+            // Act
+            string result = Triangle.ValidRightTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("Triangle is not Right Triangle", result);
+        }
+
+    }
+    }
+
